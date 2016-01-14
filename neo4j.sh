ad7560642dd888882360265f81fa11af023addaf
David Soto <dsoto@tpipr.com>
2016-01-14 13:20:58 -0400

    Initial commit

---
+++ neo4j.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+echo "Start adding Neo4j settings"
+
+echo "Downloading Neo4j..."
+
+wget http://neo4j.com/artifact.php?name=neo4j-community-2.3.1-unix.tar.gz
+
+tar xf artifact.php?name=neo4j-community-2.3.1-unix.tar.gz
+
+mv neo4j-community-2.3.1 ~/neo4j
+
+rm artifact.php?name=neo4j-community-2.3.1-unix.tar.gz
+
+echo -e "########### SETTINGS FOR HOMESTEAD ############" >> ~/neo4j/conf/neo4j-server.properties
+echo -e "org.neo4j.server.webserver.address=0.0.0.0" >> ~/neo4j/conf/neo4j-server.properties
+echo -e "###################################" >> ~/neo4j/conf/neo4j-server.properties
