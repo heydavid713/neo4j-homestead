a1e1fd930f5c7185baaf61e365ff94c788feed0c
David <ddineroxx@gmail.com>
2016-05-30 16:23:22 -0400

    Change settings to Neo4J 3.0.1

--- neo4j.sh
+++ neo4j.sh
@@ -4,21 +4,21 @@ echo "Start adding Neo4j settings"
 
 echo "Downloading Neo4j..."
 
-wget http://neo4j.com/artifact.php?name=neo4j-community-2.3.4-unix.tar.gz
+wget http://neo4j.com/artifact.php?name=neo4j-community-3.0.1-unix.tar.gz
 
-tar xf artifact.php?name=neo4j-community-2.3.4-unix.tar.gz
+tar xf artifact.php?name=neo4j-community-3.0.1-unix.tar.gz
 
-mv neo4j-community-2.3.4 ~/neo4j
+mv neo4j-community-3.0.1 ~/neo4j
 
-rm artifact.php?name=neo4j-community-2.3.4-unix.tar.gz
+rm artifact.php?name=neo4j-community-3.0.1-unix.tar.gz
 
-echo -e "########### SETTINGS FOR HOMESTEAD ############" >> ~/neo4j/conf/neo4j-server.properties
-echo -e "org.neo4j.server.webserver.address=0.0.0.0" >> ~/neo4j/conf/neo4j-server.properties
-echo -e "###################################" >> ~/neo4j/conf/neo4j-server.properties
-
+sudo add-apt-repository ppa:openjdk-r/ppa -y
 sudo apt-get update
-sudo apt-get install openjdk-7-jre -y
+sudo apt-get install openjdk-8-jre -y
+
+replace "#dbms.connector.http.address=0.0.0.0:7474" "dbms.connector.http.address=0.0.0.0:7474" -- ~/neo4j/conf/neo4j.conf
+replace "# dbms.connector.bolt.address=0.0.0.0:7687" "dbms.connector.bolt.address=0.0.0.0:7687" -- ~/neo4j/conf/neo4j.conf/
 
 ~/neo4j/bin/neo4j start
 
-echo -e "Neo4J setup is complete"
+echo -e "Neo4J setup is complete. Starting in background."
